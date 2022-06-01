5d1cb9ebb9881143d1212298bcbc6456066425a1
zmin9 <o3o0903@gmail.com>
2022-06-01 15:09:46 +0900

    add: add basic structure
    - pull하고 flutter pub get 필수(image때문)

---
+++ lib/day_file_box.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+// import 'package:life_is_an_egg/schedule.dart';
+
+class FileBox extends StatefulWidget {
+  const FileBox({Key? key}) : super(key: key);
+
+  @override
+  State<FileBox> createState() => _FileBoxState();
+}
+
+class _FileBoxState extends State<FileBox> {
+  List _navPageImg = <String>['images/nav_on.png','images/nav_off.png', 'images/nav_off.png'];
+  int curPage = 0;
+
+  void navigate(int p){
+
+    setState(() {
+      List _temp = [..._navPageImg];
+      for(int i=0;i<3;i++){
+        if(i==p) {
+          _temp[i] = 'images/nav_on.png';
+          curPage = i;
+        }
+        else {
+          _temp[i] = 'images/nav_off.png';
+        }
+      }
+      _navPageImg = _temp;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Transform.translate(offset: const Offset(0, 3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                  onTap: (){
+                    navigate(0);
+                  },
+                  child:  Stack(
+                      children: [
+                        Image.asset(_navPageImg[0], width: 122),
+                        Padding(padding: EdgeInsets.fromLTRB(33, 8, 10, 6),
+                            child: Text('schedule')),
+                      ]
+                  )
+              ),
+              GestureDetector(
+                  onTap: (){
+                    navigate(1);
+                  },
+                  child:  Stack(
+                      children: [
+                        Image.asset(_navPageImg[1], width: 122),
+                        Padding(padding: EdgeInsets.fromLTRB(43, 8, 10, 6),
+                            child: Text('health')),
+                      ]
+                  )
+              ),
+              GestureDetector(
+                  onTap: (){
+                    navigate(2);
+                  },
+                  child:  Stack(
+                      children: [
+                        Image.asset(_navPageImg[2], width: 122),
+                        Padding(padding: EdgeInsets.fromLTRB(33, 8, 10, 6),
+                            child: Text('reflection')),
+                      ]
+                  )
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 365,
+          color: Color.fromARGB(255, 255, 246, 222),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child:(
+                curPage==0?
+                /**** ScheduleList() ****/ null :(
+                    curPage==1?
+                    /**** 여기에 null 대신 Health ****/ null  : (
+                        curPage==2?
+                        /**** 여기에 null 대신 reflection ****/ null
+                            :null
+                    )
+                )
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
